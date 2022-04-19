e021f033356a8dae5b2c7452b0bf45132572b4bb
michael-valenti <48532713+michael-valenti@users.noreply.github.com>
2022-04-18 20:08:39 -0400

    Create a Note file that contains a title and content message.

---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+//REACT jsx naming convention is first letter capitalized
+//for functions
+
+function Note() {
+  return <div className="note"><h1>Title</h1> <p>content</p></div>;
+}
+
+export default Note;
