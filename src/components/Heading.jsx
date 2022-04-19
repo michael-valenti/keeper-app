45c21343d25f8aab8c52feb200c3114ae0e9cedb
michael-valenti <48532713+michael-valenti@users.noreply.github.com>
2022-04-18 20:07:41 -0400

    Create Heading file that contains the name of the app.

---
+++ src/components/Heading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+//REACT jsx naming convention is first letter capitalized
+//for functions
+
+function Heading() {
+  return <header><h1>Keeper App</h1></header>;
+}
+
+export default Heading;
