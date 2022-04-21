5171d3447284ab3ada35eb53b7e3be296c8fe538
michael-valenti <48532713+michael-valenti@users.noreply.github.com>
2022-04-20 20:23:29 -0400

    Add function to create a new Note, and then display all notes inside of the App.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,26 @@ import React from "react";
 import Heading from "./Heading";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+
+function createNote(notes){
+
+return(
+  <Note
+    key={notes.key}
+    title={notes.title}
+    body={notes.content}
+    />
+);
+
+}
 
 function App() {
     return (
       <div>
   <Heading />
-  <Note />
+      {notes.map(createNote)}
   <Footer />
       </div>
     );
