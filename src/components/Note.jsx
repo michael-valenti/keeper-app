8f33b2f2b1e3f5df3aa41f48579c607848bfda0a
michael-valenti <48532713+michael-valenti@users.noreply.github.com>
2022-04-20 20:18:48 -0400

    Add props to Note function.

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -4,8 +4,8 @@ import React from "react";
 //REACT jsx naming convention is first letter capitalized
 //for functions
 
-function Note() {
-  return <div className="note"><h1>Title</h1> <p>content</p></div>;
+function Note(props) {
+  return <div className="note"><h1>{props.title}</h1> <p>{props.body}</p></div>;
 }
 
 export default Note;
