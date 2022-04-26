f49d66e17bab3502a01fcd72b6e7fc741634c034
michael-valenti <48532713+michael-valenti@users.noreply.github.com>
2022-04-26 16:27:40 -0400

    Add a highlight icon into the header.

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-
+import HighlightIcon from '@mui/icons-material/Highlight';
 
 //REACT jsx naming convention is first letter capitalized
 //for functions
 
 function Heading() {
-  return <header><h1>Keeper App</h1></header>;
+  return <header><h1><HighlightIcon/>Keeper App</h1></header>;
 }
 
 export default Heading;
